549c93989a0566405e06126de1a664f34d94cd56
DecemberHB <27hyobin@gmail.com>
2026-03-23 16:47:26 +0900

    1차 모듈정리

---
+++ KPCompany01/src/main/java/kr/co/kpproject/kpcompany01/KpCompany01Application.java
@@ -0,0 +1,13 @@
+package kr.co.kpproject.kpcompany01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpCompany01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpCompany01Application.class, args);
+    }
+
+}
---
+++ KPCompany01/src/test/java/kr/co/kpproject/kpcompany01/KpCompany01ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kpproject.kpcompany01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpCompany01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
